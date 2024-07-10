d4243d2f4c77ba4be6ea5aa6a52828e2116bd734
Orlando <orlandoaldair199823@gmail.com>
2024-07-10 03:36:43 -0500

    Actualizar titulos

--- tests/abrirOrange.spec.ts
+++ tests/abrirOrange.spec.ts
@@ -1,16 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-/*test('abrir la web de OrangeHRM', async ({ page }) => {
-
-  //Abrir URL en el navegador
-  await page.goto('https://opensource-demo.orangehrmlive.com');
-  
-  await page.waitForTimeout(5000);
-
-});*/
-
-
-test('logueo en la web de OrangeHRM', async ({ page }) => {
+test('abrir la web de OrangeHRM', async ({ page }) => {
 
   //Abrir URL en el navegador
   await page.goto('https://opensource-demo.orangehrmlive.com');
--- tests/validarEmpleado.spec.ts
+++ tests/validarEmpleado.spec.ts
@@ -1,14 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-/*test('abrir la web de OrangeHRM', async ({ page }) => {
-
-  //Abrir URL en el navegador
-  await page.goto('https://opensource-demo.orangehrmlive.com');
-  
-  await page.waitForTimeout(5000);
-
-});*/
-
 test('validar empleado', async ({ page }) => {
 
   //Abrir URL en el navegador
